cb59fec4553b56d0afa87e3e0580222a445f7939
Xavier <xavierramosc3@gmail.com>
2025-03-17 16:58:34 +0100

    Conexion a Base de datos exitosa, avanzando middleware productos.js, Xavier

--- proyectoGrupo4/app.js
+++ proyectoGrupo4/app.js
@@ -1,8 +1,30 @@
 import express from 'express'
 import pg from 'pg'
 import cors from 'cors'
-import producto from './routes/productos'
+import producto from './routes/productos.js'
 
 const app = express()
 
+// Conexion a base de datos, si no funciona mira tu contraseña
+const dataConnection = new pg.Client({
+	user:'postgres',
+	password:'Badia123',
+	host:'localhost',
+	port: 5432,
+	database: 'supermercado',
+});
+
+dataConnection.connect()
+    .then(() => console.log('Conexion a PostgreSQL exitosa'))
+    .catch(err => console.error('Error al conectar a PostgreSQL:', err));
+
+// Configuracion de Middleware
+app.use(cors({ origin: '*' }));
+app.use(express.static('public'));
+app.use(express.json());
+
 app.use('/productos', producto)
+
+app.listen(3010, () => {
+    console.log('Servidor en http://localhost:3010');
+});
--- proyectoGrupo4/routes/productos.js
+++ proyectoGrupo4/routes/productos.js
@@ -3,7 +3,12 @@ import pg from 'pg'
 
 const route = Router();
 
+route.get('/', (req, res) => {
+    res.send('Lista de productos');
+});
 
-
+route.get('/:id', (req, res) => {
+    res.send(`Detalles del producto ${req.params.id}`);
+});
 
 export default route
