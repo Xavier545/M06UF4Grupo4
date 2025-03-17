dfc2ec6e8f41970edae23ff85a2fbaa3282dc1c0
Pablo Ortiz <pablo.ortiz.lopezyv@gmail.com>
2025-03-17 16:44:47 +0100

    Basic basic

---
+++ proyectoGrupo4/dbconnection-template.js
@@ -0,0 +1,7 @@
+const dbconnection = new Pool({
+    user: "tu_usuario",
+    host: "localhost",
+    database: "tu_base_de_datos",
+    password: "tu_contraseña",
+    port: 5432,
+});
--- proyectoGrupo4/routes/productos.js
+++ proyectoGrupo4/routes/productos.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-export default route
-import pg from pg;
+import pg from 'pg';
+import dbconnection from "../dbconnection";
 
 const route = Router();
 
-route.get('/', async (requestAnimationFrame, res) => {
-    let pgClient = new pgClient(dataConnection)
-})
+route.get('/', async (req, res) => {
+    let pgClient = new pg.Client(dbconnection)
+    await pg.Client.connect();
+    let result = await pg.Client.query("select * from Productos");
+    res.json(result.rows);
+    await pg.Client.end();
+}); 
+
+export default route;
