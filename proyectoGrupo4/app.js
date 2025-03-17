fece18cc0ec1471b96ffe16ad567e6ba06010d87
Pablo Ortiz <pablo.ortiz.lopezyv@gmail.com>
2025-03-17 17:26:29 +0100

    Change

--- proyectoGrupo4/app.js
+++ proyectoGrupo4/app.js
@@ -1,8 +1,14 @@
 import express from 'express'
 import pg from 'pg'
 import cors from 'cors'
-import producto from './routes/productos'
+import producto from './routes/productos.js'
 
 const app = express()
 
-app.use('/productos', producto)
+app.use("/productos", producto);
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+});
--- proyectoGrupo4/routes/productos.js
+++ proyectoGrupo4/routes/productos.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import pg from 'pg';
-import dbconnection from "../dbconnection";
+import dbconnection from "../dbconnection.js";
 
 const route = Router();
 
 route.get('/', async (req, res) => {
-    let pgClient = new pg.Client(dbconnection)
-    await pg.Client.connect();
-    let result = await pg.Client.query("select * from Productos");
+    let pgClient = new pg.Client(dbconnection);
+    await pgClient.connect();
+    let result = await pgClient.query("SELECT * FROM Productos");
     res.json(result.rows);
-    await pg.Client.end();
+    await pgClient.end();
 }); 
 
 export default route;
